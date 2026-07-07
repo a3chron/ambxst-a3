59742277fe160c575c504b80c9468824e84b2e6f
a3chron <kurt.schambach@gmail.com>
2026-07-07 21:42:23 +0200

    fix: adjust to local

--- config/defaults/ai.js
+++ config/defaults/ai.js
@@ -1,5 +1,5 @@
 var data = {
-    "systemPrompt": "You are a helpful assistant running on a Linux system. You have access to some tools to control the system.",
+    "systemPrompt": "You are a helpful assistant running on a NixOS system. You have access to some tools to control the system.",
     "tool": "none",
     "extraModels": [],
     "defaultModel": "gemini-2.0-flash",
--- config/defaults/weather.js
+++ config/defaults/weather.js
@@ -1,6 +1,6 @@
 .pragma library
 
 var data = {
-    "location": "",
+    "location": "Munich",
     "unit": "C"
 }
